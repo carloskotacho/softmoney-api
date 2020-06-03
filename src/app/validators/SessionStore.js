b7fe628e7d8a2273f864620f6a1366f012ffb4d3
Carlos K <carloskotachoamd@hotmail.com>
2020-06-02 21:35:06 -0300

    Update: session validator

--- src/app/validators/SessionStore.js
+++ src/app/validators/SessionStore.js
@@ -11,6 +11,10 @@ export default async (req, res, next) => {
       password: Yup.string().required(),
     });
 
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+
   } catch (err) {
     return res.status(BAD_REQUEST).json({ error: 'Validation fails', messages: err.inner });
   }
