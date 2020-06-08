c76df9572cda043142378c00d0f96721b564bcfc
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-07 23:42:14 -0300

    Added: validators for create categories

---
+++ src/app/validators/CategoryStore.js
@@ -0,0 +1,19 @@
+import { BAD_REQUEST } from 'http-status-codes';
+
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(BAD_REQUEST)
+      .json({ error: 'Validations fails', messages: err.inner });
+  }
+};
