fe16470fb334dfacf4abc8ed5fb16b6708d02d40
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-07 23:48:33 -0300

    Added: routes for create categories

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import CategoryController from './app/controllers/CategoryController';
 
 import validateUserStore from './app/validators/UserStore';
 import validateUserUpdate from './app/validators/UserUpdate';
 import validateSessionStore from './app/validators/SessionStore';
+import validateCategoryStore from './app/validators/CategoryStore';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -17,5 +19,7 @@ routes.post('/sessions', validateSessionStore, SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', validateUserUpdate, UserController.update);
+
+routes.post('/categories', validateCategoryStore, CategoryController.store);
 
 export default routes;
