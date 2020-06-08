ef7ad81d9d771f00bf5f9c7a4edda8788cf06ae9
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-08 11:21:49 -0300

    Added: list categories

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -3,6 +3,18 @@ import { BAD_REQUEST } from 'http-status-codes';
 import Category from '../models/Category';
 
 class CategoryController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const categories = await Category.findAll({
+      limit: 20,
+      offset: (page - 1) * 20,
+      attributes: ['id', 'name'],
+    });
+
+    return res.json(categories);
+  }
+
   async store(req, res) {
     const categoryExists = await Category.findOne({
       where: { name: req.body.name },
