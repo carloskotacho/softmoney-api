59fdf60c320115ce78310d2245ba3e4fa34f15ef
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-10 14:45:32 -0300

    Added: city model

--- src/database/index.js
+++ src/database/index.js
@@ -3,9 +3,10 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import Category from '../app/models/Category';
 import State from '../app/models/State';
+import City from '../app/models/City';
 import databaseConfig from '../config/database';
 
-const models = [User, Category, State];
+const models = [User, Category, State, City];
 
 class Database {
   constructor() {
