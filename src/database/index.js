d64e10cd092649667bdddad1329c7e396106467c
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-10 14:13:18 -0300

    Added: state model

--- src/database/index.js
+++ src/database/index.js
@@ -2,9 +2,10 @@ import Sequelize from 'sequelize';
 
 import User from '../app/models/User';
 import Category from '../app/models/Category';
+import State from '../app/models/State';
 import databaseConfig from '../config/database';
 
-const models = [User, Category];
+const models = [User, Category, State];
 
 class Database {
   constructor() {
