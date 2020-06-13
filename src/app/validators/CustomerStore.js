db874c3056bdcfb68bb2aac7dfeed97ecb52df83
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-12 21:56:32 -0300

    Added: validators for create customers

---
+++ src/app/validators/CustomerStore.js
@@ -0,0 +1,25 @@
+import { BAD_REQUEST } from 'http-status-codes';
+
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street: Yup.string().required(),
+      number: Yup.string().required(),
+      neighborhood: Yup.string().required(),
+      post_code: Yup.string().required(),
+      city_id: Yup.number().required(),
+      active: Yup.boolean().required(),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(BAD_REQUEST)
+      .json({ error: 'Validations fails', messages: err.inner });
+  }
+};
