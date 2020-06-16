d9461bc057f528f31a243fa637729be986c50a15
carloskotacho <carloskotachoamd@hotmail.com>
2020-06-16 15:34:05 -0300

    Added: controller for customer update

--- src/app/controllers/CustomerController.js
+++ src/app/controllers/CustomerController.js
@@ -1,3 +1,5 @@
+import { NOT_FOUND } from 'http-status-codes';
+
 import Customer from '../models/Customer';
 
 class CustomerController {
@@ -22,6 +24,38 @@ class CustomerController {
       post_code,
       city_id,
       active,
+    });
+  }
+
+  async update(req, res) {
+    const customer = await Customer.findByPk(req.params.id);
+
+    if (!customer) {
+      return res.status(NOT_FOUND).json({ error: 'Customer not found' });
+    }
+
+    const {
+      id,
+      name,
+      street,
+      number,
+      neighborhood,
+      post_code,
+      city_id,
+      active,
+      avatar_id,
+    } = await customer.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      street,
+      number,
+      neighborhood,
+      post_code,
+      city_id,
+      active,
+      avatar_id,
     });
   }
 }
